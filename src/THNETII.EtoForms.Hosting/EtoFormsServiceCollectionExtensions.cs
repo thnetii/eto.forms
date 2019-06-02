79e7b2f48f76cf9c51d9250e9fd64843063ece6e
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-06-02 23:51:43 +0200

    Add Eto.Forms Hosting extensions project

---
+++ src/THNETII.EtoForms.Hosting/EtoFormsServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using THNETII.EtoForms.Hosting;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class EtoFormsServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEtoFormsHost(IServiceCollection services)
+        {
+            services.AddSingleton(_ => Eto.Platform.Detect);
+            services.AddSingleton<Eto.Forms.Application>();
+            services.AddHostedService<EtoFormsApplicationHost>();
+
+            return services;
+        }
+    }
+}
