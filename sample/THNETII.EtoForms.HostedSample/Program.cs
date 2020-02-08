e1fc822280682561eeee5c02016f16fd37774d62
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-02-08 11:11:51 +0100

    Reorganize Gtk samples into own folder and consolidate TFMs

--- sample/THNETII.EtoForms.HostedSample/Program.cs
+++ sample/THNETII.EtoForms.HostedSample/Program.cs
@@ -7,17 +7,10 @@ namespace THNETII.EtoForms.HostedSample
     public static class Program
     {
         [STAThread]
-#if NETCOREAPP
         public static void Main(string[] args)
         {
             Host.CreateDefaultBuilder(args)
                 .RunEtoForm<MainForm>();
         }
-#else
-        public static void Main()
-        {
-            new HostBuilder().RunEtoForm<MainForm>();
-        }
-#endif
     }
 }
