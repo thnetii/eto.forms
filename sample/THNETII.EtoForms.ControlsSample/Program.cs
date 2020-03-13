1188801e8ad4eff78772c17f5831e6d6e0baf2bd
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-03-13 18:14:40 +0100

    Add empty Controls library project and samples

---
+++ sample/THNETII.EtoForms.ControlsSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace THNETII.EtoForms.ControlsSample
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
